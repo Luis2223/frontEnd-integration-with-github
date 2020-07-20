818d28362ee60bfb1c9df87e438e9e22680eb110
Luis2223 <luis.santos@grupozelo.com>
2020-07-20 17:27:01 -0300

    Carregando dados da api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Repository({ match }) {
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+import api from '../../services/api';
+
+export default class Repository extends Component {
+    state = {
+        repository: {},
+        issues: [],
+        loading: true,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const [repository, issues] = await Promise.all([
+            await api.get(`/repos/${repoName}`),
+            await api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5,
+                },
+            }),
+        ]);
+
+        this.setState({
+            loading: false,
+            repository: repository.data,
+            issues: issues.data,
+        });
+    }
+
+    render() {
+        const { repository, issues, loading } = this.state;
+        return <h1>Repository:</h1>;
+    }
 }
